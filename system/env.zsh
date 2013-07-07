22886d0923d7d6a394a3f375ff3e85d5891e8395
bsima <bensima@gmail.com>
2013-07-07 11:13:59 -0400

    general clean up and added vim plugins, config

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,9 @@
-export EDITOR='subl'
+PERL5LIB=${PERL5LIB}:${HOME}/bio/bioperl-1.2.3
+PERL5LIB=${PERL5LIB}:${HOME}/bio/ensembl/modules
+PERL5LIB=${PERL5LIB}:${HOME}/bio/ensembl-compara/modules
+PERL5LIB=${PERL5LIB}:${HOME}/bio/ensembl-variation/modules
+PERL5LIB=${PERL5LIB}:${HOME}/bio/ensembl-functgenomics/modules
+export PERL5LIB
+
+
+export EDITOR='vim'
--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/lib:/usr/lb/lightdm/lightdm:/usr/sin:/usr/bin:/sbin:/bin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
