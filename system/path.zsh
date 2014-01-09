906f2db0a56ac997474458ada5727e8915ace10f
bsima <bensima@gmail.com>
2014-01-08 20:41:20 -0500

    removed vim. Conflicts with spf13 vim config, which I like better

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
-export PATH="$HOME/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/lib:/usr/lb/lightdm/lightdm:/usr/sin:/usr/bin:/sbin:/bin:$HOME/.sfs:$ZSH/bin:$PATH"
+# export PATH="$HOME/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/lib:/usr/lb/lightdm/lightdm:/usr/sin:/usr/bin:/sbin:/bin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
