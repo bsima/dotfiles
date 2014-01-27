c5d3738341d201d449014f270e05a96ec8f16fc8
Ben Sima <bensima@gmail.com>
2014-01-27 12:20:23 -0500

    added Amazon EC2 tools

--- system/env.zsh
+++ system/env.zsh
@@ -6,3 +6,11 @@ PERL5LIB=${PERL5LIB}:${HOME}/bio/ensembl-functgenomics/modules
 export PERL5LIB
 
 export EDITOR='vim'
+
+# ## Amazon AMI Tools ##
+#
+# Not sure if this is the best place for it but whatevs.
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.4.0.9/libexec"
