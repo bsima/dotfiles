961c30c4d913ff4bf9d7817e69b033a11eba9a22
Ben Sima <bensima@gmail.com>
2014-02-02 15:09:05 -0500

    amazon AMI stuff

--- system/env.zsh
+++ system/env.zsh
@@ -11,6 +11,6 @@ export EDITOR='vim'
 #
 # Not sure if this is the best place for it but whatevs.
 export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
-export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/*.pem | /usr/bin/head -1)"
+#export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
 export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.4.0.9/libexec"
