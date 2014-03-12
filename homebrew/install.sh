be5d01956f9ac4639c4b19add95f1d2fa83c60e1
Ben Sima <bensima@gmail.com>
2014-03-12 10:51:10 -0400

    updated homebrew defaults and added documentation

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -14,6 +14,24 @@ then
 fi
 
 # Install homebrew packages
-brew install grc coreutils spark wget vim git ranger tree zsh ack drip autojump
+#
+# grc          - Generic Colouriser is yet another colouriser (written in python) for beautifying your logfiles or output of commands
+#                  For the impatient - try following commands:
+#                      grc netstat
+#                      grc ping hostname
+#                      grc tail /var/log/syslog
+#                      grc ps aux
+# coreutils    - Basic file, shell and text manipulation utilities of the GNU operating system
+# wget         - Why doesn't OSX ship with this?
+# vim          - Might as well use brew to manage my vim
+# git          - Might as well use brew to manage my git, too
+# ranger       - Python-based file-browser in your terminal
+# tree         - A cool commandline utility for visualizing directory structures
+# zsh          - Zshell, managed by brew
+# ack          - Like grep, but better for programmers 
+# drip         - Makes the JVM start superfast
+# autojump     - A tool for jumping around dirs, I don't really know how to use it yet...
+# transmission - Commandline utils for Transmission.app BitTorrent client
+brew install grc coreutils spark wget vim git ranger tree zsh ack drip autojump transmission
 
 exit 0
