d73050b4f988ae283dbdd03d75ab2068aeccd5c7
bsima <bensima@gmail.com>
2014-09-23 10:35:14 -0700

    setup emacs client properly

--- system/path.zsh
+++ system/path.zsh
@@ -1,5 +1,5 @@
 # export PATH="$HOME/.bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:/usr/lib:/usr/lb/lightdm/lightdm:/usr/sin:/usr/bin:/sbin:/bin:$HOME/.sfs:$ZSH/bin:$PATH"
 
-export PATH="./bin:$HOME/.composer/vendor/bin/:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/Applications/Mathematica.app/SystemFiles/Links/JLink/JLink.jar:/Users/bsima/Code/clojuratica/target/clojuratica-2.0.jar:$PATH"
+export PATH="./bin:$HOME/.composer/vendor/bin/:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/homebrew-cask/Caskroom/emacs/24.3/Emacs.app/Contents/MacOS/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
