87aea7046ff07cd0f576891d0983339315e6a1f9
bsima <bensima@gmail.com>
2014-11-10 17:41:24 -0500

    changes to boot, lein, and cleanup files

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.composer/vendor/bin/:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/homebrew-cask/Caskroom/emacs/24.3/Emacs.app/Contents/MacOS/bin:$HOME/Library/Haskell/bin:$PATH"
+export PATH="./bin:$HOME/.composer/vendor/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/opt/homebrew-cask/Caskroom/emacs/24.3/Emacs.app/Contents/MacOS/bin:$HOME/Library/Haskell/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
