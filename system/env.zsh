98691404509593e2b5ed40cacfa9eee24913940e
Ben Sima <bensima@gmail.com>
2015-03-01 23:02:27 -0500

    Add nix package manager to my environment

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,7 @@
-export EDITOR='mate'
+export EDITOR='emacs'
+
+# Add nix to my shell environment
+if [ -e /home/bsima/.nix-profile/etc/profile.d/nix.sh ]; then . /home/bsima/.nix-profile/etc/profile.d/nix.sh; fi
 
 
 SHELLY_HOME=/Users/bsima/.shelly; [ -s "$SHELLY_HOME/lib/shelly/init.sh" ] && . "$SHELLY_HOME/lib/shelly/init.sh"
