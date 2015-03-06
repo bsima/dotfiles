635565147c3137ab221aa74ee1c0995284c1eaf6
Ben Sima <bensima@gmail.com>
2015-03-05 21:06:03 -0500

    Add linux support

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,7 @@
-# Pipe my public key to my clipboard. Fuck you, pay me.
-alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+# Pipe my public key to my clipboard
+
+if [ "$(uname -s)" == "Darwin" ]; then
+   alias pubkey="more ~/.ssh/id_dsa.public | pbcopy | echo '=> Public key copied to pasteboard.'"
+else
+   alias pubkey="xclip -sel clip < ~/.ssh/id_rsa.pub | echo '=> Public key copied to pasteboard.'"
+fi
