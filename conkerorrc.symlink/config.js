af30769fa1e896d74e825869f60eb0df4eb93a37
Ben Sima <bensima@gmail.com>
2015-06-12 09:52:29 -0400

    Add conkeror config

---
+++ conkerorrc.symlink/config.js
@@ -0,0 +1,18 @@
+
+
+// List my history in the find-url prompt
+url_completion_use_history = true;
+
+// firebug lite
+define_variable("firebug_url",
+    "http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js");
+
+function firebug (I) {
+    var doc = I.buffer.document;
+    var script = doc.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', firebug_url);
+    script.setAttribute('onload', 'firebug.init();');
+    doc.body.appendChild(script);
+}
+interactive("firebug", "open firebug lite", firebug);
